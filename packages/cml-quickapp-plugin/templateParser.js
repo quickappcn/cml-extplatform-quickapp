75a19d468f716c8285da5b52464d26cff86676fb
80250483 <80250483@adc.com>
2019-05-21 22:12:42 +0800

    basicly set up the quickapp constructure

--- packages/cml-quickapp-plugin/templateParser.js
+++ packages/cml-quickapp-plugin/templateParser.js
@@ -1,4 +1,9 @@
-const {cmlparse, generator, types: t, traverse} = require('mvvm-template-parser');
+const {
+  cmlparse,
+  generator,
+  types: t,
+  traverse
+} = require("mvvm-template-parser");
 
 module.exports = function(content) {
   let ast = cmlparse(content);
@@ -7,17 +12,21 @@ module.exports = function(content) {
       let node = path.node;
       if (t.isJSXElement(node)) {
         let attributes = node.openingElement.attributes;
-        attributes.forEach(attr=>{
-          if(t.isJSXIdentifier(attr.name) && attr.name.name === 'c-for') {
-            attr.name.name = 'wx:for'
+        attributes.forEach(attr => {
+          if (t.isJSXIdentifier(attr.name) && attr.name.name === "c-for") {
+            attr.name.name = "for";
           }
-          if(t.isJSXIdentifier(attr.name) && attr.name.name === 'c-if') {
-            attr.name.name = 'wx:if'
+          if (t.isJSXIdentifier(attr.name) && attr.name.name === "c-if") {
+            attr.name.name = "if";
           }
-        })
+        });
         let tagName = node.openingElement.name.name;
-        if(/^origin\-/.test(tagName)) {
-          let newtagName = tagName.replace(/^origin\-/,'');
+        if (/^origin\-/.test(tagName)) {
+          let newtagName = tagName.replace(/^origin\-/, "");
+          node.openingElement.name.name = newtagName;
+          node.closingElement.name.name = newtagName;
+        } else if (tagName === "view") {
+          let newtagName = "div";
           node.openingElement.name.name = newtagName;
           node.closingElement.name.name = newtagName;
         }
@@ -25,4 +34,4 @@ module.exports = function(content) {
     }
   });
   return generator(ast).code;
-}
+};
