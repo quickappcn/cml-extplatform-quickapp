814ee7b9e96ef88b1a1e7391fe436baec2c41dde
yylgit <394720943@qq.com>
2019-05-28 10:05:39 +0800

    框架完成

--- packages/cml-quickapp-plugin/pack/copyDefault.js
+++ packages/cml-quickapp-plugin/pack/copyDefault.js
@@ -4,6 +4,5 @@ const path = require('path');
 var defaultDir = path.join(__dirname, '../default');
 const cmlUtils = require('chameleon-tool-utils');
 module.exports = function(compiler) {
-  cmlUtils.fse.copySync(path.join(__dirname,'../default'), path.join(compiler))
-
+  cmlUtils.fse.copySync(path.join(__dirname,'../default'), path.join(compiler.outputPath))
 }
