4c9663c5c28cebb2ea99192b05dfe7ac3dbff2db
zheyizhifeng <zheyizhifeng@126.com>
2019-06-12 10:32:05 +0800

    enrich api

--- packages/cml-quickapp-api/src/interfaces/px2cpx/index.js
+++ packages/cml-quickapp-api/src/interfaces/px2cpx/index.js
@@ -1,13 +1,21 @@
-import instance from './getWidth.interface';
+// import instance from './getWidth.interface';
 
-export default  function px2cpx(px) {
+// function getWidth() {
+//   return new Promise((resolve, reject) => {
+//     quickapp.device.getInfo({
+//       success: res => resolve(res.screenWidth),
+//       fail: err => reject(err)
+//     })
+//   })
+// }
+// export default async function px2cpx(px) {
 
-  if (typeof px !== 'number') {
-    console.error('Parameter must be a number');
-    return;
-  }
+//   if (typeof px !== 'number') {
+//     console.error('Parameter must be a number');
+//     return;
+//   }
 
-  const viewportWidth =  instance.getWidth();
-  const cpx = +(750 / viewportWidth * px).toFixed(3);
-  return cpx;
-}
+//   const viewportWidth = await getWidth();
+//   const cpx = +(750 / viewportWidth * px).toFixed(3);
+//   return cpx;
+// }
