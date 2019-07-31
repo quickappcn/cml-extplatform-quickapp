df5a3640c42b55a8bbc50294df38dc757e8923da
lucida <zheyizhifeng@126.com>
2019-07-31 18:52:20 +0800

    fix api named px2cpx

--- packages/cml-quickapp-api/src/interfaces/px2cpx/index.js
+++ packages/cml-quickapp-api/src/interfaces/px2cpx/index.js
@@ -1,21 +1,13 @@
-// import instance from './getWidth.interface';
+import instance from './getWidth.interface';
 
-// function getWidth() {
-//   return new Promise((resolve, reject) => {
-//     quickapp.device.getInfo({
-//       success: res => resolve(res.screenWidth),
-//       fail: err => reject(err)
-//     })
-//   })
-// }
-// export default async function px2cpx(px) {
+export default function px2cpx(px) {
 
-//   if (typeof px !== 'number') {
-//     console.error('Parameter must be a number');
-//     return;
-//   }
+  if (typeof px !== 'number') {
+    console.error('Parameter must be a number');
+    return;
+  }
 
-//   const viewportWidth = await getWidth();
-//   const cpx = +(750 / viewportWidth * px).toFixed(3);
-//   return cpx;
-// }
+  const viewportWidth = instance.getWidth();
+  const cpx = +(750 / viewportWidth * px).toFixed(3);
+  return cpx;
+}
