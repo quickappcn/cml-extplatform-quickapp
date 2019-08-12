e3e8b57e71a205ce75ad3ba3a1b71ad116e1f672
lucida <zheyizhifeng@126.com>
2019-08-12 19:54:56 +0800

    update cml-demo and fix difference

--- packages/cml-quickapp-project/src/store/actions.js
+++ packages/cml-quickapp-project/src/store/actions.js
@@ -1,3 +1,5 @@
+import index  from './index/actions'
 
 export default {
+  ...index
 }
---
+++ packages/cml-quickapp-project/src/store/yanxuan/index.js
@@ -0,0 +1,12 @@
+import actions from './actions'
+import getters from './getters'
+import state from './state'
+import mutations from './mutations'
+import createStore from "cml-quickapp-store";
+
+export default createStore({
+  actions,
+  getters,
+  state,
+  mutations
+})
