ec0d600247108f78a1fef042ccb8bc5853098e5c
lucida <zheyizhifeng@126.com>
2019-08-22 17:24:28 +0800

    update c-picker

--- packages/cml-quickapp-ui/assets/js/utils.js
+++ packages/cml-quickapp-ui/assets/js/utils.js
@@ -1,33 +1,33 @@
 import cml from 'cml-quickapp-api'
 
 export function obj2str(obj = {}) {
-    return Object.keys(obj)
-        .map(key => `${key}:${obj[key]}`)
-        .join(";");
+  return Object.keys(obj)
+    .map(key => `${key}:${obj[key]}`)
+    .join(";");
 }
 
 
 export function pxTransform(s) {
-    if (!s) return '';
-    if (!!~s.indexOf('cpx')) {
-        return s.replace(/(-?\d*\.?\d*)cpx/ig, (matchs, $1) => {
-            return cml.cpx2px(Number($1)) + 'px';
-        });
-    }
-    return s;
+  if (!s) return '';
+  if (!!~s.indexOf('cpx')) {
+    return s.replace(/(-?\d*\.?\d*)cpx/ig, (matchs, $1) => {
+      return cml.cpx2px(Number($1)) + 'px';
+    });
+  }
+  return s;
 }
 
-export function cmlStyleTransfer (str) {
-    if (!str) return {};
-    let styleAry = str.split(';');
-    let obj = {};
-    styleAry.forEach(element => {
-        let classObj = element.split(':');
-        let className = String(classObj[0]).replace(/(^\s*)|(\s*)$/g,"");
-        if (className) {
-            let classValue = pxTransform(String(classObj[1]).replace(/(^\s*)|(\s*)$/g,""));
-            obj[className] = classValue;
-        }
-    });
-    return obj;
+export function cmlStyleTransfer(str) {
+  if (!str) return {};
+  let styleAry = str.split(';');
+  let obj = {};
+  styleAry.forEach(element => {
+    let classObj = element.split(':');
+    let className = String(classObj[0]).replace(/(^\s*)|(\s*)$/g, "");
+    if (className) {
+      let classValue = pxTransform(String(classObj[1]).replace(/(^\s*)|(\s*)$/g, ""));
+      obj[className] = classValue;
+    }
+  });
+  return obj;
 }
