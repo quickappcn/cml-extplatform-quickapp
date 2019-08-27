9affd272c774e0b8aaac32427fb2cc8aa7ab6c88
lucida <zheyizhifeng@126.com>
2019-08-28 00:03:43 +0800

    update tagParser

--- packages/cml-quickapp-plugin/parser/tagParser.js
+++ packages/cml-quickapp-plugin/parser/tagParser.js
@@ -2,31 +2,37 @@ const {
   types: t,
 } = require("mvvm-template-parser");
 
+const {
+  isDivComp,
+  isBlockCom
+} = require('../util')
+
 module.exports = function (context) {
   let {
     tagName,
     node,
     attributes,
-    EMPTYTAG,
+    EMPTYTAG
   } = context;
+  let newtagName = ''
   // origin-tag原生组件替换
   if (/^origin\-/.test(tagName)) {
-    let newtagName = tagName.replace(/^origin\-/, '');
-    node.openingElement.name.name = newtagName;
-    node.closingElement.name.name = newtagName;
+    newtagName = tagName.replace(/^origin\-/, '');
   }
   // 部分组件替换
-  if (tagName === 'cover-view' || tagName === 'view' || tagName === 'scroller') {
-    let newtagName = 'div';
-    node.openingElement.name.name = newtagName;
-    node.closingElement.name.name = newtagName;
+  if (isDivComp(tagName)) {
+    newtagName = 'div';
+  }
+  if (isBlockCom(tagName)) {
+    newtagName = 'block';
   }
   if (tagName === 'cell') {
-    let newtagName = 'list-item';
+    newtagName = 'list-item';
+  }
+  if (newtagName !== '') {
     node.openingElement.name.name = newtagName;
     node.closingElement.name.name = newtagName;
   }
-
   if (tagName === 'cml-buildin-page') {
     const hasFlag = attributes.find(attr => attr.name.name === '__page__')
     if (!hasFlag) {
@@ -34,9 +40,9 @@ module.exports = function (context) {
       node.openingElement.name.name = newtagName;
       node.closingElement.name.name = newtagName;
       node.children = [t.jsxElement(t.jsxOpeningElement(t.jsxIdentifier(tagName), attributes.concat([t.jsxAttribute(t.jsxIdentifier(`__page__`), t.stringLiteral('true'))])), t.jsxClosingElement(t.jsxIdentifier(tagName)), node.children, [])]
-      attributes.forEach(attr => {
-        attr.name.name = EMPTYTAG
-      });
+      // attributes.forEach(attr => {
+      //   attr.name.name = EMPTYTAG
+      // });
     }
   }
 }
